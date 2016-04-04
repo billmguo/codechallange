9a97b7e451b9c0364ff7d4dea5ad7a66815353d0
min <billmguo@gmail.com>
2016-04-04 14:28:05 -0700

    check integer overflow

---
+++ check_integer_overflow.cpp
@@ -0,0 +1,18 @@
+ass Solution {
+public:
+    int reverse(int x) {
+         // Write your code here
+        int rst = 0;
+        
+        while(x != 0){
+            int next_rst = rst * 10 + x % 10;
+            x = x / 10;
+            if(next_rst/10 != rst) {
+                rst  = 0;
+                break;
+            }
+            rst = next_rst;
+        }
+        return rst;
+    }
+};
