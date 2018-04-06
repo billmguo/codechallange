36edeb24e5d8db0bfbeaffb62651c97e78cf258f
billmguo <billmguo@gmail.com>
2018-04-06 10:05:25 -0700

    Update IPprepare2.cpp

--- fb/IPprepare2.cpp
+++ fb/IPprepare2.cpp
@@ -160,3 +160,27 @@ double findMaxAverage(vector<int>& nums, int k) {
         }
         return left;
     }
+
+A sequence of number is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, these are arithmetic sequence:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic.
+
+1, 1, 2, 5, 7
+    
+int numberOfArithmeticSlices(vector<int>& A) {
+        int res = 0;
+        int n = A.size();
+        vector<int> dp(n, 0);
+        for (int i = 2 ; i < n; i++) {
+            if (A[i] - A[i-1] == A[i-1] - A[i-2])
+                dp[i] = dp[i-1] + 1;
+            res += dp[i];    
+        }
+        return res;
+    }
+    
