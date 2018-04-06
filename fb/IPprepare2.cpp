9643992eb8f9d17e011430893886aa2d5dc554b9
billmguo <billmguo@gmail.com>
2018-04-06 10:16:44 -0700

    Update IPprepare2.cpp

--- fb/IPprepare2.cpp
+++ fb/IPprepare2.cpp
@@ -184,3 +184,40 @@ int numberOfArithmeticSlices(vector<int>& A) {
         return res;
     }
     
+A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, these are arithmetic sequences:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic.
+
+1, 1, 2, 5, 7
+
+A zero-indexed array A consisting of N numbers is given. A subsequence slice of that array is any sequence of integers (P0, P1, ..., Pk) such that 0 ≤ P0 < P1 < ... < Pk < N.
+
+A subsequence slice (P0, P1, ..., Pk) of array A is called arithmetic if the sequence A[P0], A[P1], ..., A[Pk-1], A[Pk] is arithmetic. In particular, this means that k ≥ 2.
+
+The function should return the number of arithmetic subsequence slices in the array A.
+
+The input contains N integers. Every integer is in the range of -231 and 231-1 and 0 ≤ N ≤ 1000. The output is guaranteed to be less than 231-1.
+
+   int numberOfArithmeticSlices(vector<int>& A) {
+        int res = 0, n = A.size();
+        vector<unordered_map<int, int>> dp(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long diff = (long long)A[i] - (long long)A[j];
+                if (diff > INT_MAX || diff < INT_MIN) continue;
+                int d = (int)diff;
+                if (!dp[i].count(d)) dp[i][d] = 0;
+                ++dp[i][d];
+                if (dp[j].count(d)) {
+                    dp[i][d] += dp[j][d];
+                    res += dp[j][d];
+                }
+            }
+        }
+        return res;
+    }
