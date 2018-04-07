d8299ad62864b6c4ed35671da0b4c28d85f49ba1
billmguo <billmguo@gmail.com>
2018-04-07 15:52:05 -0700

    Update scramble.cpp

--- fb/scramble.cpp
+++ fb/scramble.cpp
@@ -19,3 +19,45 @@ public:
         return false;
     }
 };
+
+
+Sorted list to binary tree 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *sortedListToBST(ListNode *head) {
+        if (!head) return NULL;
+        if (!head->next) return new TreeNode(head->val);
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *last = slow;
+        while (fast->next && fast->next->next) {
+            last = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        fast = slow->next;
+        last->next = NULL;
+        TreeNode *cur = new TreeNode(slow->val);
+        if (head != slow) cur->left = sortedListToBST(head);
+        cur->right = sortedListToBST(fast);
+        return cur;
+    }
+};
