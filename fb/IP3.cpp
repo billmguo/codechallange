3eec68c93bf5eda6dc8ad46dd536a4f413e6edd4
billmguo <billmguo@gmail.com>
2018-04-09 22:28:15 -0700

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -1,4 +1,11 @@
-
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = INT_MAX;
+        while (mask & num) mask <<= 1;
+        return ~mask & ~num;
+    }
+};
 
 
 class Solution {
