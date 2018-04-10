663be6c006ee6e1c71ab16f3422282f6845dc094
billmguo <billmguo@gmail.com>
2018-04-10 14:47:07 -0700

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -361,3 +361,63 @@ public:
         return dp[m][n];
     }
 };
+
+
+[LeetCode] Construct Binary Tree from String 
+ 
+
+You need to construct a binary tree from a string consisting of parenthesis and integers.
+
+The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the 
+root value and a pair of parenthesis contains a child binary tree with the same structure.
+
+You always start to construct the left child node of the parent first if it exists.
+
+Example:
+
+Input: "4(2(3)(1))(6(5))"
+Output: return the tree root node representing the following tree:
+
+       4
+     /   \
+    2     6
+   / \   / 
+  3   1 5 
+
+class Solution {
+public:
+    TreeNode* str2tree(string s) {
+        if (s.empty())
+            return NULL:
+        stack<TreeNode*> st;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == ')') st.pop();
+            else if ((s[i] >= '0' && s[i] <= '9') || s[i] == '-') {
+                int j = i;
+                while (i + 1 < s.size() && s[i + 1] >= '0' && s[i + 1] <= '9') ++i;
+                TreeNode *cur = new TreeNode(stoi(s.substr(j, i - j + 1)));
+                if (!st.empty()) {
+                    TreeNode *t = st.top();
+                    if (!t->left) t->left = cur;
+                    else t->right = cur;
+                }
+                st.push(cur);
+            }
+        }
+        return st.top();
+    }
+};
+
+
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if (!t) return "";
+        string res = to_string(t->val);
+        if (!t->left && !t->right)
+            return res;
+        res += "(" + tree2str(t->left) +")";
+        if (t->right) res += "(" + tree2str(t->right) + ")";
+        return res;
+    }
+};
