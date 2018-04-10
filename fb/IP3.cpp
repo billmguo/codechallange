e38cb216ede630777369b18cc83ecfa26d9cf8df
billmguo <billmguo@gmail.com>
2018-04-09 21:52:50 -0700

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -241,3 +241,23 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        if (n == 0) return -1;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (A[mid] == target) return mid;
+            else if (A[mid] < A[right]) {
+                if (A[mid] < target && A[right] >= target) left = mid + 1;
+                else right = mid - 1;
+            } else {
+                if (A[left] <= target && A[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
