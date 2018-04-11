074235574bbc3925b2249445c6a940c58202f40b
billmguo <billmguo@gmail.com>
2018-04-10 17:58:21 -0700

    Update ip_6.cpp

--- fb/ip_6.cpp
+++ fb/ip_6.cpp
@@ -109,3 +109,24 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> sums = nums;
+        for (int i = 1; i < n; ++i) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+        double res = (double)sums[k - 1] / k;
+        for (int i = k; i < n; ++i) {
+            double t = sums[i];
+            if (t > res * (i + 1)) res = t / (i + 1);
+            for (int j = i - k; j >= 0; --j) {
+                t = sums[i] -  sums[j];
+                if (t > res * (i - j)) res = t / (i - j);
+            }
+        }
+        return res;
+    }
+};
