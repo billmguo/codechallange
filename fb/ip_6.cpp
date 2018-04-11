2fe4b96332e53ca5800410dd92fe822d468418d7
billmguo <billmguo@gmail.com>
2018-04-10 17:37:19 -0700

    Create ip_6.cpp

---
+++ fb/ip_6.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+   int newInteger(int n) {
+        int res = 0, base = 1;
+        while (n > 0) {
+            res += n % 9 * base;
+            n /= 9;
+            base *= 10;
+        }
+        return res;
+   }
+};
+Maximum Size Subarray Sum Equals k
+ int maxSubArrayLen(vector<int>& nums, int k) {
+        int res = 0;
+        int sum  = 0;
+        unordered_map<int, int> m{{0,-1}};
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (m.count(sum - k)) {
+                res = max(res, i - m[sum - k]);
+            }
+            if (!m.count(sum))
+                m[sum] = i;
+        }
+        return res;
+    }
+
+if (n <= 8) return n;
+return 10 * newInteger(n / 9) + (n % 9)
+
+while (num!= 0 && cnt++ < 8) {
+	res = str[(num & 0xf)] + res;
+	num >>= 4;
+}
+return res.empty()? "0":res;
+
+
+bool wordPattern(string pattern, string str) {
+        unordered_map<char, int> m1;
+        unordered_map<string, int> m2;
+        istringstream in(str);
+        int i = 0;
+        for (string word; i >> word; i++) {
+        	if (m1.count(pattern[i]) || m2.count(word))
+        		if (m1[pattern[i]] != m2[word])
+        			return false;
+        	else{
+        		m1[pattern[i]] = i + 1;
+        		m2[word] = i + 1;
+        	}
+
+        }
+        return i == pattern.size();
+    }
+
+
+
+ class Solution {
+public:
+    bool wordPatternMatch(string pattern, string str) {
+        unordered_map<char, string> m;
+        set<string> s;
+        return helper(pattern, 0, str, 0, m, s);
+    }
+    bool helper(string pattern, int p, string str, int r, unordered_map<char, string> &m, set<string> &s) {
+        if (p == pattern.size() && r == str.size()) return true;
+        if (p == pattern.size() || r == str.size()) return false;
+        char c = pattern[p];
+        for (int i = r; i < str.size(); ++i) {
+            string t = str.substr(r, i - r + 1);
+            if (m.count(c) && m[c] == t) {
+                if (helper(pattern, p + 1, str, i + 1, m, s)) return true;
+            } else if (!m.count(c)) {
+            	if (s.count(t))
+            		continue;
+            	if (s.count(t)) continue;
+                m[c] = t;
+                s.insert(t);
+                if (helper(pattern, p + 1, str, i + 1, m, s)) return true;
+                m.erase(c);
+                s.erase(t);
+            }
+        }
+        return false;
+    }
+};
