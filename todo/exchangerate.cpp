6d470a3c1239c198fa5e0f165a1d1644064cae26
billmguo <billmguo@gmail.com>
2019-05-09 21:03:42 -0700

    Create exchangerate.cpp

---
+++ todo/exchangerate.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
+        vector<double> res;
+        for (int i = 0; i < equations.size(); ++i) {
+            m[equations[i].first][equations[i].second] = values[i];
+            m[equations[i].second][equations[i].first] = 1.0 / values[i];
+        }
+        for (auto query : queries) {
+            unordered_set<string> visited;
+            double t = helper(query.first, query.second, visited);
+            res.push_back((t > 0.0) ? t : -1);
+        }
+        return res;
+    }
+    double helper(string up, string down, unordered_set<string>& visited) {
+        if (m[up].count(down)) return m[up][down];
+        for (auto a : m[up]) {
+            if (visited.count(a.first)) continue;
+            visited.insert(a.first);
+            double t = helper(a.first, down, visited);
+            if (t > 0.0) return t * a.second;
+        }
+        return -1.0;
+    }
+
+private:
+    unordered_map<string, unordered_map<string, double>> m;
+};
