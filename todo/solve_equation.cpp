59fc0152b920da7c5da19613665b78f9c8d1cde8
billmguo <billmguo@gmail.com>
2019-05-19 21:37:31 -0700

    Update solve_equation.cpp

--- todo/solve_equation.cpp
+++ todo/solve_equation.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string solveEquation(string equation) {
-        int n = equation.size(), a = 0, b = 0, sign = 1, j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (equation[i] == '+' || equation[i] == '-') {
-                if (i > j) b += stoi(equation.substr(j, i - j)) * sign;
-                j = i;
-            } else if (equation[i] == 'x') {
-                if (i == j || equation[i - 1] == '+') {
-                    a += sign;                 
-                } else if (equation[i - 1] == '-') {
-                    a -= sign;
-                } else {
-                    a += stoi(equation.substr(j, i - j)) * sign;
-                }
-                j = i + 1;
-            } else if (equation[i] == '=') {
-                if (i > j) b += stoi(equation.substr(j, i - j)) * sign;
-                sign = -1;
-                j = i + 1;
-            }
-        }
-        if (j < n) b += stoi(equation.substr(j)) * sign;
+        int a = 0, b = 0;
+        auto found = equation.find("=");
+        helper(equation.substr(0, found), true, a, b);
+        helper(equation.substr(found + 1), false, a, b);
         if (a == 0 && a == b) return "Infinite solutions";
         if (a == 0 && a != b) return "No solution";
-        return "x=" + to_string(-b / a);
+        return "x=" + to_string(b / a);
+    }
+    void helper(string e, bool isLeft, int& a, int& b) {
+        int sign = 1, num = -1;
+        e += "+";
+        for (int i = 0; i < e.size(); ++i) {
+            if (e[i] == '-' || e[i] == '+') {
+                num = (num == -1) ? 0 : (num * sign);
+                b += isLeft ? -num : num;
+                num = -1;
+                sign = (e[i] == '+') ? 1 : -1;
+            } else if (e[i] >= '0' && e[i] <= '9') {
+                if (num == -1) num = 0;
+                num = num * 10 + e[i] - '0';
+            } else if (e[i] == 'x') {
+                num = (num == -1) ? sign : (num * sign);
+                a += isLeft ? num : -num;
+                num = -1;
+            }
+        }
     }
 };
