09babd000a5d21087a3f387e1a3860b1c88e9b9f
billmguo <billmguo@gmail.com>
2019-06-11 13:37:41 -0700

    Update closest.cpp

--- in/closest.cpp
+++ in/closest.cpp
@@ -55,3 +55,24 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        auto cmp = [](vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
+        for (vector<int> &point:points) {
+            pq.push(point);
+            if (pq.size() > K)
+                pq.pop();
+        }
+        vector<vector<int>> ans; 
+        while(!pq.empty()) {
+                ans.push_back(pq.top());
+                pq.pop();
+        }
+        return ans;
+    }
+};
