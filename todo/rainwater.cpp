2a1bfc7a289e621e6f1aaf40ab3ecf086a82c161
billmguo <billmguo@gmail.com>
2019-06-18 21:42:52 -0700

    Create rainwater.cpp

---
+++ todo/rainwater.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, mx = 0, n = height.size();
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = mx;
+            mx = max(mx, height[i]);
+        }
+        mx = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = min(dp[i], mx);
+            mx = max(mx, height[i]);
+            if (dp[i] > height[i]) res += dp[i] - height[i];
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size() - 1, level = 0, res = 0;
+        while (l < r) {
+            int lower = height[(height[l] < height[r]) ? l++ : r--];
+            level = max(level, lower);
+            res += level - lower;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        int i = 0, res = 0, n = height.size();
+        while (i < n) {
+            if (st.empty() || height[i] <= height[st.top()]) {
+                st.push(i++);
+            } else {
+                int t = st.top(); st.pop();
+                if (st.empty()) continue;
+                res += (min(height[i], height[st.top()]) - height[t]) * (i - st.top() - 1);
+            }
+        }
+        return res;
+    }
+};
