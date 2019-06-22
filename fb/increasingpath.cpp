9ebe913c67c75c8e7351c1e73b85af678c8711e9
billmguo <billmguo@gmail.com>
2019-06-21 19:57:59 -0700

    Update increasingpath.cpp

--- fb/increasingpath.cpp
+++ fb/increasingpath.cpp
@@ -33,3 +33,25 @@ public:
         return max(left,right);
     }
 };
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+        int res = 0;
+        helper(root, root, res);
+        return res;
+    }
+    pair<int, int> helper(TreeNode* node, TreeNode* parent, int& res) {
+        if (!node) return {0, 0};
+        auto left = helper(node->left, node, res);
+        auto right = helper(node->right, node, res);
+        res = max(res, left.first + right.second + 1);
+        res = max(res, left.second + right.first + 1);
+        int inc = 0, dec = 0;
+        if (node->val > parent->val ) {
+            inc = max(left.first, right.first) + 1;
+        } else if (node->val < parent->val) {
+            dec = max(left.second, right.second) + 1;
+        }
+        return {inc, dec};
+    }
+};
