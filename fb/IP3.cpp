69133348d5d36d6c7591c9d2b6c17745e6b3a138
billmguo <billmguo@gmail.com>
2019-06-25 18:33:25 -0700

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -1,3 +1,42 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int> > res;
+        vector<int> out;
+        sort(S.begin(), S.end());
+        dfs(S, 0, out, res);
+        return res;
+    }
+    void dfs(vector<int> &S, int pos, vector<int> &out, vector<vector<int> > &res) {
+        if (out.size() >= 2) {
+            if (*min_element(out) + *max_element(out) == target)
+                res.push_back(out);
+        }
+        res.push_back(out);
+        for (int i = pos; i < S.size(); ++i) {
+            out.push_back(S[i]);
+            dfs(S, i + 1, out, res);
+            out.pop_back();
+        }
+    }
+};
 /*sub two string*/
 #include <iostream>
 #include <string>
