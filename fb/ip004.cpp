b74207bf2c8a32687b1cb069983a7b52ab54da7b
billmguo <billmguo@gmail.com>
2019-06-25 19:16:53 -0700

    Update ip004.cpp

--- fb/ip004.cpp
+++ fb/ip004.cpp
@@ -506,4 +506,27 @@ public:
     }
 };
 
-
+class Solution {
+public:
+    Node* treeToDoublyList(Node* root) {
+        if (!root) return NULL;
+        Node *head = NULL, *pre = NULL;
+        inorder(root, pre, head);
+        pre->right = head;
+        head->left = pre;
+        return head;
+    }
+    void inorder(Node* node, Node*& pre, Node*& head) {
+        if (!node) return;
+        inorder(node->left, pre, head);
+        if (!head) {
+            head = node;
+            pre = node;
+        } else {
+            pre -> right = node;
+            node-> left = pre;
+            pre = node;
+        }
+        inorder(node->right, pre, head);
+    }
+};
