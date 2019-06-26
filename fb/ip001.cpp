60e3b105fb9a783a07a3fd3d0554ff77f94004cc
billmguo <billmguo@gmail.com>
2019-06-25 22:15:19 -0700

    Update ip001.cpp

--- fb/ip001.cpp
+++ fb/ip001.cpp
@@ -1,3 +1,26 @@
+Your are given an array of positive integers nums.
+
+Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+
+Example 1:
+Input: nums = [10, 5, 2, 6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+        int res = 0, prod = 1, left = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            prod *= nums[i];
+            while (prod >= k) prod /= nums[left++];
+            res += i - left + 1;
+        }
+        return res;
+    }
+};
+
 Given an integer array A, you partition the array into (contiguous) 
 subarrays of length at most K.  After partitioning, each subarray has 
 their values changed to become the maximum value of that subarray.
