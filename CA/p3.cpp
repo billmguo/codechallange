809fb12f6e45c471950ab0a7ddc6fcf50333b78a
billmguo <billmguo@gmail.com>
2019-07-10 11:41:46 -0700

    Update p3.cpp

--- CA/p3.cpp
+++ CA/p3.cpp
@@ -1,3 +1,46 @@
+[LeetCode] Word Ladder 词语阶梯
+ 
+/*
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+Note:
+
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+*/
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord)) return 0;
+        queue<string> q{{beginWord}};
+        int res = 0;
+        while (!q.empty()) {
+            for (int k = q.size(); k > 0; --k) {
+                string word = q.front(); q.pop();
+                if (word == end) return res + 1;
+                for (int i = 0; i < word.size(); ++i) {
+                    string newWord = word;
+                    for (char ch = 'a'; ch <= 'z'; ++ch) {
+                        newWord[i] = ch;
+                        if (wordSet.count(newWord) && newWord != word) {
+                            q.push(newWord);
+                            wordSet.erase(newWord);
+                        }   
+                    }
+                }
+            }
+            ++res;
+        }
+        return 0;
+    }
+};
 [LeetCode] 296. Best Meeting Point 最佳开会地点
  
 
