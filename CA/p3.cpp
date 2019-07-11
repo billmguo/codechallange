2e4165292206018f0e8da5fc10070c76fd37b75e
billmguo <billmguo@gmail.com>
2019-07-11 09:39:23 -0700

    Update p3.cpp

--- CA/p3.cpp
+++ CA/p3.cpp
@@ -258,3 +258,28 @@ public:
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+Rotate K group 
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *dummy = new ListNode(-1), *pre = dummy, *cur = pre;
+        dummy->next = head;
+        int num = 0;
+        while (cur = cur->next) ++num;
+        while (num >= k) {
+            cur = pre->next;
+            for (int i = 1; i < k; ++i) {
+                ListNode *t = cur->next;
+                cur->next = t->next;
+                t->next = pre->next;
+                pre->next = t;
+            }
+            pre = cur;
+            num -= k;
+        }
+        return dummy->next;
+    }
+};
