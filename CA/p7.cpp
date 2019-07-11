00049a5bfbf7cd3aa8a2981ce1627382778f91cd
billmguo <billmguo@gmail.com>
2019-07-11 15:32:16 -0700

    Update p7.cpp

--- CA/p7.cpp
+++ CA/p7.cpp
@@ -30,6 +30,73 @@ public:
     }
 };
 
+[LeetCode] LFU Cache 最近最不常用页面置换缓存器
+ 
+
+Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LFUCache cache = new LFUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.get(3);       // returns 3.
+cache.put(4, 4);    // evicts key 1.
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+ 
+
+ class LFUCache {
+public:
+    LFUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if (m.count(key) == 0) return -1;
+        freq[m[key].second].erase(iter[key]);
+        ++m[key].second;
+        freq[m[key].second].push_back(key);
+        iter[key] = --freq[m[key].second].end();
+        if (freq[minFreq].size() == 0) ++minFreq;
+        return m[key].first;
+    }
+    
+    void put(int key, int value) {
+        if (cap <= 0) return;
+        if (get(key) != -1) {
+            m[key].first = value;
+            return;
+        }
+        if (m.size() >= cap) {
+            m.erase(freq[minFreq].front());
+            iter.erase(freq[minFreq].front());
+            freq[minFreq].pop_front();
+        }
+        m[key] = {value, 1};
+        freq[1].push_back(key);
+        iter[key] = --freq[1].end();
+        minFreq = 1;
+    }
+
+private:
+    int cap, minFreq;
+    unordered_map<int, pair<int, int>> m;
+    unordered_map<int, list<int>> freq;
+    unordered_map<int, list<int>::iterator> iter;
+};
+
 [LeetCode] Number of Islands II 岛屿的数量之二
  
 
