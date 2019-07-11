ca32954e383b983a2cb58f39f14b4263b5125180
billmguo <billmguo@gmail.com>
2019-07-10 18:28:18 -0700

    Update p3.cpp

--- CA/p3.cpp
+++ CA/p3.cpp
@@ -1,3 +1,54 @@
+/*A frog is crossing a river. The river is divided into x units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+
+Given a list of stones' positions (in units) in sorted ascending order, determine if the frog is able to cross the river by landing on the last stone. Initially, the frog is on the first stone and assume the first jump must be 1 unit.
+
+If the frog's last jump was k units, then its next jump must be either k - 1, k, or k + 1 units. Note that the frog can only jump in the forward direction.
+
+Note:
+
+The number of stones is ≥ 2 and is < 1,100.
+Each stone's position will be a non-negative integer < 231.
+The first stone's position is always 0.
+Example 1:
+
+[0,1,3,5,6,8,12,17]
+
+There are a total of 8 stones.
+The first stone at the 0th unit, second stone at the 1st unit,
+third stone at the 3rd unit, and so on...
+The last stone at the 17th unit.
+
+Return true. The frog can jump to the last stone by jumping 
+1 unit to the 2nd stone, then 2 units to the 3rd stone, then 
+2 units to the 4th stone, then 3 units to the 6th stone, 
+4 units to the 7th stone, and 5 units to the 8th stone.
+ */
+
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+       
+        if (stones.empty())
+            return false;
+        map<int, set<int>> m;
+        for (int i = 0; i < stones.size(); ++i) {
+            m[stones[i]] = set<int>();
+        }
+        m[0].insert(0);
+        for (int i = 0; i < stones.size(); i++) {
+            for (int step:m[stones[i]]) {
+                for (int nexts = step - 1; nexts <= step + 1; nexts++) {
+                    if (nexts && m.count(stones[i] + nexts)) {
+                        m[stones[i] + nexts].insert(nexts);
+                    }
+                }
+            }
+        }
+        return !m[stones.back()].empty();
+    }
+};
+
+
 [LeetCode] Word Ladder 词语阶梯
  
 /*
