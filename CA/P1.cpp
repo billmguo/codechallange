51319b7a5fbdd06fdc47605bf5cfcaff7d4ec27c
billmguo <billmguo@gmail.com>
2019-07-11 18:19:58 -0700

    Update P1.cpp

--- CA/P1.cpp
+++ CA/P1.cpp
@@ -113,6 +113,61 @@ public:
 Design a circular queue
 
 class MyCircularQueue {
+private:
+    vector<int> q;
+    int len, head, tail;
+public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    MyCircularQueue(int k) {
+        q = vector<int>(k, 0);
+        head = tail = 0;
+        len = k;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    bool enQueue(int value) {
+        if(isFull()) 
+            return false;
+        q[tail%len]=value;
+        ++tail;
+        return true;
+        
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if(isEmpty())
+            return false;
+        ++head;
+        return true;
+    }
+    
+    /** Get the front item from the queue. */
+    int Front() {
+        if(isEmpty()) 
+            return -1;
+        return q[head%len];
+    }
+    
+    /** Get the last item from the queue. */
+    int Rear() {
+        if(isEmpty()) 
+            return -1;
+        return q[(tail-1)%len];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    bool isEmpty() {
+        return (head == tail);
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    bool isFull() {
+        return (tail- head == len);
+    }
+};
+
+class MyCircularQueue {
 public:
     MyCircularQueue(int k) {
         size = k; head = k - 1; tail = 0; cnt = 0;
