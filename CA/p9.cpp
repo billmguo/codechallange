4c2babf4115af03a2b4a7b0d87838b0d1a9e0565
billmguo <billmguo@gmail.com>
2019-07-11 23:44:24 -0700

    Update p9.cpp

--- CA/p9.cpp
+++ CA/p9.cpp
@@ -80,3 +80,21 @@ public:
         return dp[n];
     }
 };
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        vector<string> res;
+        vector<string> dict{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        dfs(digits, dict, 0, "", res);
+        return res;
+    }
+    void dfs(string& digits, vector<string>& dict, int level, string out, vector<string>& res) {
+        if (level == digits.size()) 
+           {res.push_back(out); return;}
+        string str = dict[digits[level] - '0'];
+        for (int i = 0; i < str.size(); ++i) {
+            dfs(digits, dict, level + 1, out + str[i], res);
+        }
+    }
+};
