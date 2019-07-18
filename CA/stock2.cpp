50418c7a37d7bcd69364f9b5130ebd7d8de48679
billmguo <billmguo@gmail.com>
2019-07-18 16:31:33 -0700

    Update stock2.cpp

--- CA/stock2.cpp
+++ CA/stock2.cpp
@@ -1,84 +1,119 @@
-https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108870/most-consistent-ways-of-dealing-with-the-series-of-stock-problems
-Say you have an array for which the ith element is the price of a given stock on day i.
+Circual dequeue 
 
-Design an algorithm to find the maximum profit. You may complete at most two transactions.
-
-Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
-
-我们其实可以求至少k次交易的最大利润。我们定义local[i][j]为在到达第i天时最多可进行j次交易并且最后一次交易在最后一天卖出的最大利润，
-此为局部最优。然后我们定义global[i][j]为在到达第i天时最多可进行j次交易的最大利润，此为全局最优。它们的递推式为：
-
-local[i][j] = max(global[i - 1][j - 1] + max(diff, 0), local[i - 1][j] + diff)
-
-global[i][j] = max(local[i][j], global[i - 1][j])，
-class Solution {
+class MyCircularDeque {
 public:
-    int maxProfit(vector<int> &prices) {
-        if (prices.empty()) return 0;
-        int n = prices.size(), g[n][3] = {0}, l[n][3] = {0};
-        for (int i = 1; i < prices.size(); ++i) {
-            int diff = prices[i] - prices[i - 1];
-            for (int j = 1; j <= 2; ++j) {
-                l[i][j] = max(g[i - 1][j - 1] + max(diff, 0), l[i - 1][j] + diff);
-                g[i][j] = max(l[i][j], g[i - 1][j]);
-            }
-        }
-        return g[n - 1][2];
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    MyCircularDeque(int k) {
+        size = k; head = k - 1; tail = 0, cnt = 0;
+        data.resize(k);
     }
+    
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    bool insertFront(int value) {
+        if (isFull()) return false;
+        data[head] = value;
+        head = (head - 1 + size) % size;
+        ++cnt;
+        return true;
+    }
+    
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    bool insertLast(int value) {
+        if (isFull()) return false;
+        data[tail] = value;
+        tail = (tail + 1)  % size;
+        ++cnt;
+        return true;
+    }
+    
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    bool deleteFront() {
+        if (isEmpty()) return false;
+        head = (head + 1) % size;
+        --cnt;
+        return true;
+    }
+    
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    bool deleteLast() {
+        if (isEmpty()) return false;
+        tail = (tail - 1 + size) % size;
+        --cnt;
+        return true;
+    }
+    
+    /** Get the front item from the deque. */
+    int getFront() {
+        return isEmpty() ? -1 : data[(head + 1) % size];
+    }
+    
+    /** Get the last item from the deque. */
+    int getRear() {
+        return isEmpty() ? -1 : data[(tail - 1 + size) % size];
+    }
+    
+    /** Checks whether the circular deque is empty or not. */
+    bool isEmpty() {
+        return cnt == 0;
+    }
+    
+    /** Checks whether the circular deque is full or not. */
+    bool isFull() {
+        return cnt == size;
+    }
+
+private:
+    vector<int> data;
+    int size, head, tail, cnt;
 };
 
-Say you have an array for which the ith element is the price of a given stock on day i.
+[LeetCode] Design Circular Queue 设计环形队列
 
-Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
-
-You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
-After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
-
-
-buy[i]  = max(rest[i-1] - price, buy[i-1]) 
-sell[i] = max(buy[i-1] + price, sell[i-1])
-rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
-
-
-rest[i] = sell[i-1]
-
-buy[i]  = max(sell[i-2] - price, buy[i-1]) 
-sell[i] = max(buy[i-1] + price, sell[i-1])
-
-
-class Solution {
+class Circualqueue{
+	int size;
+	int head, tail, cnt;
 public:
-    int maxProfit(vector<int>& prices) {
-        int buy = INT_MIN, pre_buy = 0, sell = 0, pre_sell = 0;
-        for (int price : prices) {
-            pre_buy = buy;
-            buy = max(pre_sell - price, pre_buy);
-            pre_sell = sell;
-            sell = max(pre_buy + price, pre_sell);
-        }
-        return sell;
-    }
-};
-
-[LeetCode] Best Time to Buy and Sell Stock with Transaction Fee 买股票的最佳时间含交易费
- 
-
-Your are given an array of integers prices, for which the i-th element is the
- price of a given stock on day i; and a non-negative integer fee representing a transaction fee.
-
-sold[i] = max(sold[i - 1], hold[i - 1] + prices[i] - fee);
-
-hold[i] = max(hold[i - 1], sold[i - 1] - prices[i]);
-
-class Solution {
+	Circualqueue(int k) {
+		size = k; head = k - 1;
+		tail = 0; cnt = 0;
+		data.resize(k);
+	}
+}
+class MyCircularQueue {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        vector<int> sold(prices.size(), 0), hold = sold;
-        hold[0] = -prices[0];
-        for (int i = 1; i < prices.size(); ++i) {
-            sold[i] = max(sold[i - 1], hold[i - 1] + prices[i] - fee);
-            hold[i] = max(hold[i - 1], sold[i - 1] - prices[i]);
-        }
-        return sold.back();
+    MyCircularQueue(int k) {
+        size = k; head = k - 1;
+        tail = 0; cnt = 0;
+        data.resize(k);
     }
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        data
+        data[tail] = value;
+        tail = (tail + 1) % size;
+        ++cnt;
+        return true;
+    }
+    bool deQueue() {
+        if (isEmpty()) return false;
+        head = (head + 1) % size;
+        --cnt;
+        return true;
+    }
+    int Front() {
+        return isEmpty() ? -1 : data[(head + 1) % size];
+    }
+    int Rear() {
+        return isEmpty() ? -1 : data[(tail - 1 + size) % size];
+    }
+    bool isEmpty() {
+        return cnt == 0;
+    }
+    bool isFull() {
+        return cnt == size;
+    }
+    
+private:
+    vector<int> data;
+    int size, cnt, head, tail;
 };
