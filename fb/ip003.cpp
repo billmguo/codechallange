4aff617ef9ffff0effa485d229f13bc82ac8e5f0
billmguo <billmguo@gmail.com>
2021-01-16 12:31:11 -0800

    Update ip003.cpp

--- fb/ip003.cpp
+++ fb/ip003.cpp
@@ -1,3 +1,26 @@
+You are given a series of video clips from a sporting event that lasted T seconds.  These video clips can be overlapping with each other and have varied lengths.
+
+Each video clip clips[i] is an interval: it starts at time clips[i][0] and ends at time clips[i][1].  We can cut these clips into segments freely: for example, a clip [0, 7] can be cut into segments [0, 1] + [1, 3] + [3, 7].
+
+Return the minimum number of clips needed so that we can cut the clips into segments that cover the entire sporting event ([0, T]).  If the task is impossible, return -1.
+
+class Solution {
+public:
+    int videoStitching(vector<vector<int>>& clips, int T) {
+        sort(begin(clips), end(clips));
+        int res = 0;
+       
+
+        for (int i = 0, st = 0, end =0; st < T; st = end, ++res) {
+          while(i < clips.size() && clips[i][0] <= st)
+            end = max(clips[i++],end);
+          if (st == end)
+            return -1;
+        }
+        return res;
+    }
+};
+
 Example:
 
 Input: "4(2(3)(1))(6(5))"
