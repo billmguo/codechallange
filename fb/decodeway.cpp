24276490a97bedcd8be943f9f266abf252187f4a
billmguo <billmguo@gmail.com>
2021-01-18 17:23:16 -0800

    Update decodeway.cpp

--- fb/decodeway.cpp
+++ fb/decodeway.cpp
@@ -17,3 +17,26 @@ public:
         return dp.back();
     }
 };
+
+class Solution {
+public:
+    Solution(vector<int> w) {
+        sum = w;
+        for (int i = 1; i < w.size(); ++i) {
+            sum[i] = sum[i - 1] + w[i];
+        }
+    }
+    
+    int pickIndex() {
+        int x = rand() % sum.back(), left = 0, right = sum.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (sum[mid] <= x) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+    
+private:
+    vector<int> sum;
+};
