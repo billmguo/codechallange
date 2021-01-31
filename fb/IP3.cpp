01f0a78b03c817979ce280420a9306cdbe695125
billmguo <billmguo@gmail.com>
2021-01-30 16:21:05 -0800

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -1,3 +1,20 @@
+Battleship
+
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int count = 0;
+        for(int i=0;i<board.size();++i)
+            for(int j=0;j<board[i].size();++j) {
+                if(board[i][j] == 'X'
+                   && (i==0 || board[i-1][j] == '.')
+                   && (j==0 || board[i][j-1] == '.')) 
+		   ++count;
+            }
+        return count;
+    }
+};
+
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& A, int K) {
