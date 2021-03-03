330ff3f51f7607233e0bf6aff327c2104db910fd
billmguo <billmguo@gmail.com>
2021-03-02 22:38:19 -0800

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -1,3 +1,30 @@
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: True
+Explanation:
+1234
+5123
+9512
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size() - 1; ++i) {
+            for (int j = 0; j < matrix[i].size() - 1; ++j) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) return false;
+            }
+        }
+        return true;
+    }
+};
+
+
 A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
 
 A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
