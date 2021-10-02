071627cd7ea469623df7e3b2a11190e8fede1fdb
billmguo <billmguo@gmail.com>
2021-10-02 15:48:49 -0700

    Update ip008.cpp

--- fb/ip008.cpp
+++ fb/ip008.cpp
@@ -310,3 +310,23 @@ public:
         return total / pow(8, K);
     }
 };
+
+// Non-recursion
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *p = root;
+        while (p || !s.empty()) {
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top(); s.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
