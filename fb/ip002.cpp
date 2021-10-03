b07f14d24e93dfda1e3b3b7ee57a7b81daf471dd
billmguo <billmguo@gmail.com>
2021-10-03 09:43:47 -0700

    Update ip002.cpp

--- fb/ip002.cpp
+++ fb/ip002.cpp
@@ -191,3 +191,20 @@ public:
         return (res == INT_MAX) ? -1 : res;
     }
 };
+
+/*
+interval intersection 
+*/
+
+vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> v;
+        int i=0,j=0;
+        while(i<A.size() && j<B.size()){
+            int l=max(A[i][0], B[j][0]);
+            int u=min(A[i][1], B[j][1]);
+            if(l<=u) v.push_back({l,u});
+            if(A[i][1] < B[j][1])   i++;
+            else j++;
+        }
+        return v;
+    }
