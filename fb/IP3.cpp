a49e8a8956306792309232fc4c5b278a3ce621c2
billmguo <billmguo@gmail.com>
2021-10-10 19:36:43 -0700

    Update IP3.cpp

--- fb/IP3.cpp
+++ fb/IP3.cpp
@@ -1,3 +1,50 @@
+/*There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+
+You are given two integer array position and speed, both of length n, where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
+
+A car can never pass another car ahead of it, but it can catch up to it, and drive bumper to bumper at the same speed.
+
+The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+
+A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+
+If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+
+Return the number of car fleets that will arrive at the destination.
+
+ 
+
+Example 1:
+
+Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+Output: 3
+Explanation: 
+The cars starting at 10 and 8 become a fleet, meeting each other at 12.
+The car starting at 0 doesn't catch up to any other car, so it is a fleet by itself.
+The cars starting at 5 and 3 become a fleet, meeting each other at 6.
+Note that no other cars meet these fleets before the destination, so the answer is 3.
+*/
+
+
+
+int carfleet(int target, vector<int> &pos, vector<int> &speed) {
+	vector<pid> cars(n);
+	for (int i = 0; i < n; i++) {
+		cars[i].first = pos[i];
+		cars[i].second = (target - pos[i])/speed[i];
+	}
+
+	sort(cars.begin(), cars.end());
+
+
+	int last = cars[n-1], res = 1;
+	for (int i = n - 2; i >= 0; i--) {
+		if (cars[i].second > last) {
+			ans++;
+			last = cars[i].second;
+		}
+	}
+}
 A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
 
 Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
