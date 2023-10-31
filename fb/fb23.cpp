4c82b6b4396b01c92d81400534e9a508f009de28
billmguo <billmguo@gmail.com>
2023-10-30 21:38:41 -0700

    Update fb23.cpp

--- fb/fb23.cpp
+++ fb/fb23.cpp
@@ -475,4 +475,32 @@ class Solution {
   }
 };
 
+/* An array nums of length n is beautiful if:
+
+nums is a permutation of the integers in the range [1, n].
+For every 0 <= i < j < n, there is no index k with i < k < j where 2 * nums[k] == nums[i] + nums[j].
+Given the integer n, return any beautiful array nums of length n. There will be at least one valid answer for the given n.
+
+*/
+
+class Solution {
+public:
+    vector<int> beautifulArray(int N) {
+        vector<int> res{1};
+        while (res.size() < N) {
+            vector<int> t;
+            for (int num : res) {
+                if (num * 2 - 1 <= N) t.push_back(num * 2 - 1);
+            }
+            for (int num : res) {
+                if (num * 2 <= N) t.push_back(num * 2);
+            }
+            res = t;
+        }
+        return res;
+    }
+};
+
+
+
 
