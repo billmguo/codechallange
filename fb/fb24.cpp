c3d4082cfb97630c0812a0adce1ae45eb820d2a7
billmguo <billmguo@gmail.com>
2023-10-31 10:50:20 -0700

    Update fb24.cpp

--- fb/fb24.cpp
+++ fb/fb24.cpp
@@ -1,3 +1,34 @@
+Problem
+You are given a 0-indexed array of positive integers tasks, representing 
+    tasks that need to be completed in order, where tasks[i] represents the type of the ith task.
+
+You are also given a positive integer space, which represents the minimum 
+number of days that must pass after the completion of a task before another task of the same type can be performed.
+
+Each day, until all tasks have been completed, you must either:
+
+Complete the next task from tasks, or
+
+Take a break.
+
+Return** the minimum number of days needed to complete all tasks**.
+class Solution {
+ public:
+  long long taskSchedulerII(vector<int>& tasks, int space) {
+    unordered_map<int, long long> taskToNextAvailable;
+    long long ans = 0;
+
+    for (const int task : tasks) {
+      ans = max(ans + 1, taskToNextAvailable[task]);
+      taskToNextAvailable[task] = ans + space + 1;
+    }
+
+    return ans;
+  }
+};
+
+
+
 class Solution {
 public:
     double minAreaFreeRect(vector<vector<int>>& points) {
