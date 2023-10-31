71501e81915eb5b81df3248dcc8ed049d443c56c
billmguo <billmguo@gmail.com>
2023-10-31 11:20:44 -0700

    Update fb24.cpp

--- fb/fb24.cpp
+++ fb/fb24.cpp
@@ -1,3 +1,40 @@
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and
+
+column-wise, return the number of negative numbers in grid.
+
+ class Solution {
+ public:
+  int countNegatives(vector<vector<int>>& grid) {
+    const int m = grid.size();
+    const int n = grid[0].size();
+    int ans = 0;
+    int i = m - 1;
+    int j = 0;
+
+    while (i >= 0 && j < n) {
+      if (grid[i][j] < 0) {
+        ans += n - j;
+        --i;
+      } else {
+        ++j;
+      }
+    }
+
+    return ans;
+  }
+};
+
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+
 Problem
 You are given a 0-indexed array of positive integers tasks, representing 
     tasks that need to be completed in order, where tasks[i] represents the type of the ith task.
