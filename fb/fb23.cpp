e6a76a6d61033a8ca72d1b56f2fbb8539bbb30c9
billmguo <billmguo@gmail.com>
2023-10-30 20:41:27 -0700

    Update fb23.cpp
    update

--- fb/fb23.cpp
+++ fb/fb23.cpp
@@ -1,3 +1,35 @@
+There are n people standing in a queue, and they numbered from 0 to n - 1 in 
+ left to right order. You are given an array heights of distinct integers 
+where heights[i] represents the height of the i-th person.
+ A person can see another person to their right in the queue if 
+everybody in between is shorter than both of them. More formally,
+ the i-th person can see the j-th person if i < j and min(heights[i], heights[j]) >
+max(heights[i+1], heights[i+2], ..., heights[j-1]).
+
+Return an array answer of length n where answer[i] is the number 
+of people the i-th person can see to their right in the queue.
+
+
+class Solution {
+ public:
+  vector<int> canSeePersonsCount(vector<int>& heights) {
+    const int n = heights.size();
+    vector<int> ans(n);
+    stack<int> stack;
+
+    for (int i = 0; i < n; ++i) {
+      while (!stack.empty() && heights[stack.top()] <= heights[i])
+        ++ans[stack.top()], stack.pop();
+      if (!stack.empty())
+        ++ans[stack.top()];
+      stack.push(i);
+    }
+
+    return ans;
+  }
+};
+
+
 K Radius Subarray Averages
 
 
