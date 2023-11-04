624fc348fb54b2306e05af4a21205dad67382779
billmguo <billmguo@gmail.com>
2023-11-04 08:32:33 -0700

    Update fb24.cpp

--- fb/fb24.cpp
+++ fb/fb24.cpp
@@ -131,6 +131,39 @@ vector<int>  countOfNodes(Node root, vector<pair<char,int>> queries, string s) {
     return res;
 }
 
+class Solution {
+private:
+    void dfs(vector<vector<int>> & paths, vector<int> & res,
+             vector<bool> & visited, vector<int> & times, 
+             string & labels, int curr) {
+        if (visited[curr]) {
+            return;
+        }
+        visited[curr] = true;
+        int curr_times = times[labels[curr] - 'a']++;
+        for (auto next : paths[curr]) {
+            dfs(paths, res, visited, times, labels, next);
+        }
+        res[curr] = times[labels[curr] - 'a'] - curr_times;
+    }
+public:
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>> paths(n);
+        for (auto edge : edges) {
+            paths[edge[0]].push_back(edge[1]);
+            paths[edge[1]].push_back(edge[0]);
+        }
+        
+        vector<int> res(n);
+        vector<bool> visited(n, false);
+        vector<int> times(26);
+        
+        dfs(paths, res, visited, times, labels, 0);
+        
+        return res;
+    }
+};
+
 Minimum Length Substrings
 You are given two strings s and t. You can select any substring of string s and rearrange the characters of the selected substring. Determine the minimum length of the substring of s such that string t is a substring of the selected substring.
 Signature
