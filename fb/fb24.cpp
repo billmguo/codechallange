671a2e0b6db7bc54c23a46e3cb03c127da947d4a
billmguo <billmguo@gmail.com>
2023-11-03 22:41:23 -0700

    Update fb24.cpp

--- fb/fb24.cpp
+++ fb/fb24.cpp
@@ -637,31 +637,45 @@ Explanation: [2,3,2,1,3] is also a valid sequence, but [3,1,2,3,2] is the lexico
 // Time: O(N!)
 // Space: O(N)
 class Solution {
-    vector<int> used;
-    bool dfs(vector<int> &ans, int i) {
-        if (i == ans.size()) return true; // filled all the numbers, found the answer
-        if (ans[i]) return dfs(ans, i + 1); // this index is already filled, continue to fill the next index.
-        for (int j = used.size() - 1; j > 0; --j) { // try each number in decending order from n to 1.
-            if (used[j]) continue; // j is already used, skip
-            if (j != 1 && (i + j >= ans.size() || ans[i + j])) continue; // we can't fill `ans[i + j]` either because `i + j` is out of bound or `ans[i + j]` is already filled. Skip.
-            used[j] = 1; // mark number `j` as used.
-            ans[i] = j; // fill `ans[j]` and `ans[i + j]` (if needed) with `j`.
-            if (j != 1) ans[i + j] = j;
-            if (dfs(ans, i + 1)) return true;
-            ans[i] = 0; // this filling is invalid, backtrack and try the next number.
-            if (j != 1) ans[i + j] = 0;
-            used[j] = 0;
-        }
-        return false;
+ public:
+  vector<int> constructDistancedSequence(int n) {
+    vector<int> ans(2 * n - 1);
+    dfs(n, 0, 0, ans);
+    return ans;
+  }
+
+ private:
+  bool dfs(int n, int i, int mask, vector<int>& ans) {
+    if (i == ans.size())
+      return true;
+    if (ans[i] > 0)
+      return dfs(n, i + 1, mask, ans);
+
+    // Greedily fill in descendingly.
+    for (int num = n; num >= 1; --num) {
+      if ((mask >> num & 1) == 1)
+        continue;
+      if (num == 1) {
+        ans[i] = num;
+        if (dfs(n, i + 1, mask | 1 << num, ans))
+          return true;
+        ans[i] = 0;
+      } else {  // num in [2, n]
+        if (i + num >= ans.size() || ans[i + num] > 0)
+          continue;
+        ans[i] = num;
+        ans[i + num] = num;
+        if (dfs(n, i + 1, mask | 1 << num, ans))
+          return true;
+        ans[i + num] = 0;
+        ans[i] = 0;
+      }
     }
-public:
-    vector<int> constructDistancedSequence(int n) {
-        vector<int> ans(2 * n - 1);
-        used.assign(n + 1, 0); // numbers 1 ~ n are unused initially
-        dfs(ans, 0); // try filling numbers from index 0.
-        return ans;
-    }
+
+    return false;
+  }
 };
+
 
 Two strings X and Y are similar if we can swap two letters (in different positions) of X, so that it equals Y.
 
