e57f639ac2803c5c5d0a4a090ffa6d94123194da
billmguo <billmguo@gmail.com>
2023-11-04 17:30:00 -0700

    Update fb27.cpp

--- fb/fb27.cpp
+++ fb/fb27.cpp
@@ -1,3 +1,28 @@
+/*Given an unsorted array of integers, find the length of the longest consecutive elements sequence.*/
+
+Your algorithm should run in O( n ) complexity.
+class Solution {
+ public:
+  int longestConsecutive(vector<int>& nums) {
+    int ans = 0;
+    unordered_set<int> seen{nums.begin(), nums.end()};
+
+    for (int num : nums) {
+      // Num is the start of a sequence
+      if (seen.count(num - 1))
+        continue;
+      int length = 1;
+      while (seen.count(++num))
+        ++length;
+      ans = max(ans, length);
+    }
+
+    return ans;
+  }
+};
+
+
+
 /*Description
 There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
 
